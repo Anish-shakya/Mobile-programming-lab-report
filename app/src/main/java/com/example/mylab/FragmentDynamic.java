e78a2864030211b066725b42f7018cd78c60426e
Anish Shakya <shakyaanish69@gmail.com>
2023-07-31 16:19:29 +0545

    fragment manager

--- app/src/main/java/com/example/mylab/FragmentDynamic.java
+++ app/src/main/java/com/example/mylab/FragmentDynamic.java
@@ -3,9 +3,13 @@ package com.example.mylab;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FragmentDynamic extends AppCompatActivity {
 
@@ -19,25 +23,38 @@ public class FragmentDynamic extends AppCompatActivity {
         btnFragB= findViewById(R.id.btnFragB);
         btnFragC = findViewById(R.id.btnFragC);
 
+        //Default Fragment
+        loadFrag(new AFragment(),0);
         btnFragA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadFrag(new AFragment(),1);
             }
         });
+
 
         btnFragB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadFrag(new BFragment(),1);
             }
         });
 
         btnFragC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadFrag(new CFragment(),1);
             }
         });
+    }
+    public void loadFrag(Fragment fragment, int flag){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag == 0)
+            ft.add(R.id.container,fragment);
+        else
+            ft.replace(R.id.container,fragment);
+
+        ft.commit();
     }
 }
